b70d028aa1b4e3104823b3803dac317469353818
kaaang-shin <shinhyeok.kang@dshare.co.kr>
2023-02-05 22:57:29 +0900

    commit

--- src/test/java/com/git/action/controller/UserControllerTest.java
+++ src/test/java/com/git/action/controller/UserControllerTest.java
@@ -1,20 +1,20 @@
-package com.git.action.controller;
-
-import com.git.action.config.TestBaseConfig;
-import org.junit.jupiter.api.Test;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-class UserControllerTest extends TestBaseConfig {
-    @Test
-    void test() throws Exception {
-        params.add("name","name");
-        params.add("email","email");
-
-        mockMvc
-                .perform(get("/test").params(params))
-                .andExpect(status().isOk());
-    }
-}
+//package com.git.action.controller;
+//
+//import com.git.action.config.TestBaseConfig;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//class UserControllerTest extends TestBaseConfig {
+//    @Test
+//    void test() throws Exception {
+//        params.add("name","name");
+//        params.add("email","email");
+//
+//        mockMvc
+//                .perform(get("/test").params(params))
+//                .andExpect(status().isOk());
+//    }
+//}
