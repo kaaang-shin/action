d84d322993821914e8432eda07941734e5b69090
kaaang-shin <shinhyeok.kang@dshare.co.kr>
2023-02-05 23:26:31 +0900

    commit

--- src/test/java/com/git/action/config/TestBaseConfig.java
+++ src/test/java/com/git/action/config/TestBaseConfig.java
@@ -1,26 +1,26 @@
-//package com.git.action.config;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.util.LinkedMultiValueMap;
-//import org.springframework.util.MultiValueMap;
-//
-//@ExtendWith({SpringExtension.class})
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureMockMvc(printOnlyOnFailure = false)
-//@ActiveProfiles("test")
-//@Import({TestMysqlConfig.class})
-//public abstract class TestBaseConfig {
-//    protected final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//    @Autowired protected MockMvc mockMvc;
-//
-//}
+package com.git.action.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@ExtendWith({SpringExtension.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
+@ActiveProfiles("test")
+@Import({TestMysqlConfig.class})
+public abstract class TestBaseConfig {
+    protected final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    @Autowired protected MockMvc mockMvc;
+
+}
